bde6efecbfc5be40577e9a8777cf252c36fee882
Ren-Logronio <87650672+Ren-Logronio@users.noreply.github.com>
2022-10-08 17:49:55 +0800

    Add files via upload

---
+++ Config.php
@@ -0,0 +1,10 @@
+<?php
+
+$databaseHost = 'localhost';
+$databaseName = 'ps';
+$databaseUsername = 'root';
+$databasePassword = 'pass123';
+
+$mysqli = mysqli_connect($databaseHost, $databaseUsername, $databasePassword, $databaseName);
+
+?>
